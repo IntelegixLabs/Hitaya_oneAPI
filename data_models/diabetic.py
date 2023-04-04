9e9c3b72a0812589e7213e6a20c263fde5afe197
ARNAB DAS <raj713335@gmail.com>
2023-04-04 22:49:34 +0530

    :wrench: Added Diabetic Disease prediction

---
+++ data_models/diabetic.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class diabetic (BaseModel):
+    Pregnancies: int
+    Glucose: int
+    BloodPressure: int
+    SkinThickness: int
+    Insulin: int
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
